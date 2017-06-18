ef4a51b0682919e3116073643a9e28ea4ff29aa7
bao ling koh <cocoinquire@gmail.com>
2017-06-19 01:01:57 +0800

    break

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ var columnDefs = [
     headerName: "Currency", field: "currency"
   }
 ];
+
 var rowData = [
-  {currency: "gdp", rate: "1.3"},
-  {currency: "SGD", rate: "2.4"}
+  {currency: "gdp", rate: 1.3},
+  {currency: "SGD", rate: 2.3}
 ];
+
 var gridOptions = {
   columnDefs: columnDefs,
   rowData: rowData,
@@ -24,14 +26,19 @@ var gridOptions = {
 var date = new Date(), y = date.getFullYear(), m = 0; g=date.getMonth();
 for (var i =1; i<13; i++){
 var endDate = moment(new Date(y, m+i, 0)).format('MMM Do YY')
+if (i === g+1){
 columnDefs[i] = {headerName: endDate, field: "rate"}
-}
+} else if (i>g+1){
+columnDefs[i] = {headerName: endDate, field: `future${i}_rate`}
+} else {
+columnDefs[i] = {headerName: endDate, field: `history${i}_rate`}
+}}
 for (var i = 1; i<g+1; i++){
 var apiDate = moment(new Date(y, m+i, 0)).format('YYYY-MM-DD')
 fetch(`https://openexchangerates.org/api/historical/${apiDate}.json?app_id=04d5f27b626548d69e87c07ef86057a8`)
 .then((resp) => resp.json())
 .then(function(history) {
-  
+
 })
 .catch(function() {
   console.error('error')
